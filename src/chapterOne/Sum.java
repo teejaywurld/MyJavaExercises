78021b233a9b0dda6ae4d0c178eb3c4f0db93f89
Adetunji Henry <teejaywurld@gmail.com>
2022-01-17 19:01:53 +0100

    updated code

---
+++ src/chapterOne/Sum.java
@@ -0,0 +1,10 @@
+package chapterOne;
+
+public class Sum {
+    public static void main(String[] args) {
+        int numberOne = 1;
+        int numberTwo = 6;
+        int sum = numberOne + numberTwo;
+        System.out.println(sum);
+    }
+}
